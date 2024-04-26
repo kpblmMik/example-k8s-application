f89995a14003fc23389e8962993df25f5ae99c0a
Mikhail Sarnov <kpblm2@gmail.com>
2024-04-26 11:20:08 +0200

    adding terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "Default AWS Region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "clustername" {
+  description = "example-eks"
+  type        = string
+  default     = "example-eks"
+}
